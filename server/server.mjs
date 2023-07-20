3da3d186892ed2537f555318980b01db147fb519
ananyaj <ananyaj@umich.edu>
2023-07-20 15:40:53 -0400

    removed

--- server/server.mjs
+++ server/server.mjs
@@ -1,7 +1,5 @@
 import express from "express";
 import cors from "cors";
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
 import "./loadEnvironment.mjs";
 import reviews from "./routes/review.mjs";
 import movies from "./routes/movie.mjs";
@@ -11,17 +9,9 @@ import mongoose from "mongoose";
 const PORT = process.env.PORT || 5050;
 const connectionString = process.env.ATLAS_URI || "";
 const app = express();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 app.use(cors());
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
-
-  // Catch-all route
-  app.get('*', (req, res) => {
-    res.sendFile(join(__dirname, '..', 'client', 'public', 'index.html'));
-  });
 
   mongoose.connect(connectionString, {
     useNewUrlParser: true,
